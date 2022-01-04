e5934cdd29d36728050ff6b8dd9c808a2e9f6390
Saiglor <jakimets.2012@gmail.com>
2022-01-04 16:25:33 +0500

    Добавил сервис калькулятор для вычисления платежа по кредиту

---
+++ Otus.NewFeatures.LoanCalculator/Dtos/LoanDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Otus.NewFeatures.LoanCalculator.Dtos
+{
+    //NEW FEATURE
+    public record LoanDto(double LoanAmount, ushort NumberMonths, double InterestRate);
+}
---
+++ Otus.NewFeatures.LoanCalculator/LoanCalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using Otus.NewFeatures.LoanCalculator.Dtos;
+using System;
+
+namespace Otus.NewFeatures.LoanCalculator
+{
+    public class LoanCalculatorService
+    {
+        public static double GetAnnuityPayment(LoanDto loan)
+        {
+            //NEW FEATURE
+            if (loan is null) throw new ArgumentNullException();
+
+            var monthlyInterestRate = loan.InterestRate / 1200;
+
+            var annuityRatio = monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, loan.NumberMonths) /
+                               (Math.Pow(1 + monthlyInterestRate, loan.NumberMonths) - 1);
+
+            return Math.Round(annuityRatio * loan.LoanAmount, 2);
+        }
+    }
+}
