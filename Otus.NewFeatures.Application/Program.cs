5681a477316fa09741dab6f2dddbfeef95337f6a
Saiglor <jakimets.2012@gmail.com>
2022-01-04 16:40:16 +0500

    Добавил вызов сервиса

---
+++ Otus.NewFeatures.Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Otus.NewFeatures.LoanCalculator;
+using Otus.NewFeatures.LoanCalculator.Dtos;
+
+
+//NEW FEATURE
+LoanDto loan1 = new(300000, 18, 15);
+//NEW FEATURE
+var loan2 = loan1 with { NumberMonths = 24 };
+
+var annuityPayment1 = LoanCalculatorService.GetAnnuityPayment(loan1);
+var annuityPayment2 = LoanCalculatorService.GetAnnuityPayment(loan2);
+
+Console.WriteLine($"Первый аннуитетный платеж: {annuityPayment1}");
+Console.WriteLine($"Второй аннуитетный платеж: {annuityPayment2}");
