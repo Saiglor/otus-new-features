aa28df46af1cf56f2ff079b81389f25c5fae19cb
Saiglor <jakimets.2012@gmail.com>
2022-01-04 16:41:02 +0500

    Добавил проверку значений в LoanDto

--- Otus.NewFeatures.LoanCalculator/Dtos/LoanDto.cs
+++ Otus.NewFeatures.LoanCalculator/Dtos/LoanDto.cs
@@ -1,5 +1,37 @@
-﻿namespace Otus.NewFeatures.LoanCalculator.Dtos
+﻿using System;
+
+namespace Otus.NewFeatures.LoanCalculator.Dtos
 {
     //NEW FEATURE
-    public record LoanDto(double LoanAmount, ushort NumberMonths, double InterestRate);
+    public record LoanDto
+    {
+        /// <summary>
+        /// Сумма займа
+        /// </summary>
+        public double LoanAmount { get; init; }
+
+        /// <summary>
+        /// Количество месяцев
+        /// </summary>
+        public ushort NumberMonths { get; init; }
+
+        /// <summary>
+        /// Процентная ставка
+        /// </summary>
+        public double InterestRate { get; init; }
+
+        public LoanDto(double loanAmount, ushort numberMonths, double interestRate)
+        {
+            //NEW FEATURE
+            if (numberMonths is < 3 or > 1200)
+                throw new ArgumentException("Срок займа должен быть в промежутке от 3 месяцев до 50 лет");
+            //NEW FEATURE
+            if (interestRate is < 0.01 or > 60)
+                throw new ArgumentException("Ставка по займу должна быть в диапозоне от 0.01 до 60 % годовых");
+
+            LoanAmount = loanAmount;
+            NumberMonths = numberMonths;
+            InterestRate = interestRate;
+        }
+    }
 }
